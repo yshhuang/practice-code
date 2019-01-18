5d7367b65e3e8259d3da9a3bec34c24efccd7c51
huangyongsheng <yshhuang@foxmail.com>
2019-01-18 15:49:22 +0800

    system information

---
+++ tlcl/sys_info_page.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Prage to output a system information page
+TITLE="System Information Report for $HOSTNAME"
+CURRENT_TIME=$(date +"%x %r %Z")
+TIME_STAMP="Generated $CURRENT_TIME, by $USER"
+echo "<HTML>
+	<HEAD>
+		<TITLE>$TITLE</TITLE>
+	</HEAD>
+	<BODY>
+		<H1>$TIME_STAMP</H1>
+	</BODY>
+	<HTML>"
