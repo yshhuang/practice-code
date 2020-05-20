8116ec089a0ced8e332aeae07da7a3561fe5f1ea
huangyongsheng <huangyongsheng@kkworld.com>
2020-05-20 17:27:22 +0800

    lenet

--- d2l-zh/deep-learning-compute/gpu.py
+++ d2l-zh/deep-learning-compute/gpu.py
@@ -10,6 +10,23 @@ from mxnet import nd
 from mxnet.gluon import nn
 
 if __name__ == '__main__':
-    print(mx.cpu(), mx.gpu(), mx.gpu(1))
-    x = nd.array([1, 2, 3],ctx=mx.gpu())
+    print(mx.cpu(), mx.gpu())
+    x = nd.array([1, 2, 3], ctx=mx.gpu())
     print(x.context)
+
+    y = x.copyto(mx.gpu())
+    print(y)
+
+    z = x.as_in_context(mx.gpu())
+    print(z)
+
+    print(y.as_in_context(mx.gpu()) is y)
+    print(y.copyto(mx.gpu()) is y)
+
+    print((z + 2).exp() * y)
+
+    net = nn.Sequential()
+    net.add(nn.Dense(1))
+    net.initialize(ctx=mx.gpu())
+    print(net(y))
+    print(net[0].weight.data())
