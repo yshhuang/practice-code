8ab707aa16773e16343b9be57c9a9b554dbea03c
huangyongsheng <huangyongsheng@kkworld.com>
2020-05-21 14:26:26 +0800

    batch0norm

--- d2l-zh/convolutional-neural-network/batch-norm.py
+++ d2l-zh/convolutional-neural-network/batch-norm.py
@@ -57,47 +57,51 @@ class BatchNorm(nn.Block):
 
 
 if __name__ == '__main__':
+    # net = nn.Sequential()
+    # net.add(nn.Conv2D(6, kernel_size=5),
+    #         BatchNorm(6, num_dims=4),
+    #         nn.Activation('sigmoid'),
+    #         nn.MaxPool2D(pool_size=2, strides=2),
+    #         nn.Conv2D(16, kernel_size=5),
+    #         BatchNorm(16, num_dims=4),
+    #         nn.Activation('sigmoid'),
+    #         nn.MaxPool2D(pool_size=2, strides=2),
+    #         nn.Dense(120),
+    #         BatchNorm(120, num_dims=2),
+    #         nn.Activation('sigmoid'),
+    #         nn.Dense(84),
+    #         BatchNorm(84, num_dims=2),
+    #         nn.Activation('sigmoid'),
+    #         nn.Dense(10))
+    #
+    # lr, num_epochs, batch_size, ctx = 1.0, 5, 256, d2l.try_gpu()
+    # net.initialize(ctx=ctx, init=init.Xavier())
+    # trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
+    # train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+    # d2l.train_ch5(net, train_iter, test_iter, batch_size, trainer,
+    #               ctx, num_epochs)
+    # print(net[1].gamma.data().reshape((-1,)), net[1].beta.data().reshape((-1,)))
+
     net = nn.Sequential()
     net.add(nn.Conv2D(6, kernel_size=5),
-            BatchNorm(6, num_dims=4),
+            nn.BatchNorm(),
             nn.Activation('sigmoid'),
             nn.MaxPool2D(pool_size=2, strides=2),
             nn.Conv2D(16, kernel_size=5),
-            BatchNorm(16, num_dims=4),
+            nn.BatchNorm(),
             nn.Activation('sigmoid'),
             nn.MaxPool2D(pool_size=2, strides=2),
             nn.Dense(120),
-            BatchNorm(120, num_dims=2),
+            nn.BatchNorm(),
             nn.Activation('sigmoid'),
             nn.Dense(84),
-            BatchNorm(84, num_dims=2),
+            nn.BatchNorm(),
             nn.Activation('sigmoid'),
             nn.Dense(10))
-
     lr, num_epochs, batch_size, ctx = 1.0, 5, 256, d2l.try_gpu()
-    net.initialize(ctx=ctx, init=init.Xavier())
-    trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
-    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
-    d2l.train_ch5(net, train_iter, test_iter, batch_size, trainer,
-                  ctx, num_epochs)
-    print(net[1].gamma.data().reshape((-1,)), net[1].beta.data().reshape((-1,)))
-
-    net = nn.Sequential()
-    net.add(nn.Conv2D(6, kernel_size=5),
-            nn.BatchNorm(),
-            nn.Activation('sigmoid'),
-            nn.MaxPool2D(pool_size=2, strides=2),
-            nn.Conv2D(16, kernel_size=5),
-            nn.BatchNorm(),
-            nn.Activation('sigmoid'),
-            nn.MaxPool2D(pool_size=2, strides=2),
-            nn.Dense(120),
-            nn.BatchNorm(),
-            nn.Activation('sigmoid'),
-            nn.Dense(84),
-            nn.BatchNorm(),
-            nn.Activation('sigmoid'),
-            nn.Dense(10))
     net.initialize(ctx=ctx, init=init.Xavier)
     trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': lr})
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+    d2l.train_ch5(net, train_iter, test_iter, batch_size, trainer, ctx,
+                  num_epochs)
     print(net[1].gamma.data().reshape((-1,)), net[1].beta.data().reshape((-1,)))
